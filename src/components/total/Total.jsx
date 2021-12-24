394c63fd7cdcee45bcf60265cdb91c58a19b3a97
Maksym Kovalenko <m.v.kovalenko4@gmail.com>
2021-12-24 14:59:54 +0200

    added section questions

--- src/components/total/Total.jsx
+++ src/components/total/Total.jsx
@@ -1,9 +1,43 @@
 import React from "react";
+import SectionTitle from "../../components/uikit/SectionTitle/SectionTItle";
+import Button from "../../components/uikit/Button/Button";
 import style from "./Total.module.css";
 import family from "../../images/family.png";
+import document from "../../images/icons/document.svg";
 
 const Total = () => {
-  return <section className={style.section}></section>;
+  return (
+    <section className={style.section}>
+      <SectionTitle text="Ви хочете?" />
+      <div className={style.wrapper}>
+        <div className={style.contentWrapper}>
+          <ul className={style.list}>
+            <li className={style.item}>
+              Переконатись у професіоналізмі наших лікарів?
+            </li>
+            <li className={style.item}>Протестувати наш дружній сервіс?</li>
+            <li className={style.item}>Заключити декларацію з лікарем?</li>
+            <li className={style.item}>Вакцинуватись?</li>
+            <li className={style.item}>
+              Хочете на власні очі побачити нашу клініку?
+            </li>
+            <li className={style.item}>
+              Отримати направлення до вузького спеціаліста?
+            </li>
+          </ul>
+          <Button
+            styleName={style.serviceBtn}
+            text="Записатися на прийом"
+            bgColor="blue"
+            icons={document}
+          />
+        </div>
+        <div className={style.imageWrapper}>
+          <img src={family} alt="" />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Total;
--- src/pages/mainPage/MainPage.jsx
+++ src/pages/mainPage/MainPage.jsx
@@ -4,14 +4,16 @@ import Entry from "../../components/entry/Entry";
 import Advantages from "../../components/advantages/Advantages";
 import Services from "../../components/services/Services";
 import Doctors from "../../components/doctors/Doctors";
+import Total from "../../components/total/Total";
 
 const MainPage = () => {
   return (
     <MainLayout>
       <Entry />
       <Services />
-      <Advantages />
       <Doctors />
+      <Total />
+      <Advantages />
     </MainLayout>
   );
 };
